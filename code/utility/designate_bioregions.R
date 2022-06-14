c27d4310302fffb0937d5dd1f9a7f6c7231258b7
ST <feathertailglidr@gmail.com>
2022-06-14 15:24:20 +1000

    delete stuff about sampling bias

--- code/utility/designate_bioregions.R
+++ code/utility/designate_bioregions.R
@@ -2,40 +2,16 @@
 # 2022-06-14
 
 # Map and raster
-library(leaflet); library(maps); library(mapdata); library(glue)
+library(maps); library(mapdata)
 # Read shapefiles of biome
-library(sf); library(raster)
+library(sf); library(raster); library(rgdal)
 
 
 # data --------------------------------------------------------------------
 
 qgis_df <- read.csv('data/2021_ALA_blindsnake_occurence_data/2021_blindsnake_distribution.csv')
 
-
 # Check which region they fall into ---------------------------------------
-
-# Map
-qgis_df <- qgis_df %>% 
-  tibble() %>% 
-  filter(species != "",
-         catalogue_number != "") %>% 
-  arrange(species) 
-
-# Sample bias
-## Prepare
-samptest <- qgis_df[, c('species', 'decimal_longitude_wgs84', 'decimal_latitude_wgs84')]
-names(samptest) <- c('species', 'decimalLongitude', 'decimalLatitude')
-
-# samptest$species <- gsub(pattern = ' ', replacement = '_', x = samptest$species)
-
-# Export for Infomap Bioregions 
-write.csv(x = samptest, file = 'data/20220218_occurence_ALA_cleaned.csv', row.names = FALSE)
-
-# Running sampbias
-samp.out <- sampbias::calculate_bias(x = samptest, res = 1)
-
-# summary(samp.out)
-# plot(samp.out)
 
 # Need to read this from directory where all the other files are... *.shp by itself won't work.
 wwf_biome <- raster::shapefile("data/WWF_Australia/wwf_terr_ecos.shp")
@@ -67,66 +43,3 @@ ggplot(data = top_3_ecoregions, aes(fill = ECO_NAME, y = prop, x = species)) +
 
 
 
-
-
-# tree --------------------------------------------------------------------
-
-mt_tree <- ape::read.nexus('data/tree/MCC_10_meanh.tre')
-
-# MANIPULATE TREE ---------------------------------------------------------
-# Keep only tips of species
-
-tip_keep <- c(
-  'acuticauduso', 'suboculariso', 'affinis1',
-  'wiedii92', 'ganei36', 'ligatus70',
-  'kimberleyensis65', 'troglodytes85', 'polygrammicus81',
-  'nigrescens75', 'silvia84', 'guentheri51',
-  'howi62', 'unguirostris87', 'grypus43', 'leptosoma67',
-  'leptosoma68', 'longissimus73', 'bicolor14',
-  'pinguis80', 'bituberculatus19', 'proximus83',
-  'australis13', 'endoterus35', 'hamatus60',
-  'pilbarensis79', 'centralis21', 'waitii90',
-  'ammodytes11', 'diversus26')
-
-# sub tree gets rid of number and SH
-sub_mt_tree <- mt_tree %>%
-  keep.tip(., tip_keep)
-
-sub_mt_tree$tip.label[which(sub_mt_tree$tip.label == 'leptosoma67')] <- "systenos24"
-
-# Force ultrametric 
-sub_mt_tree <- phytools::force.ultrametric(sub_mt_tree,"extend")
-
-# Clean up tip label
-sub_mt_tree$tip.label <- gsub("[0-9]+", '', sub_mt_tree$tip.label) # rename so it so we have tips labels without numbers
-sub_mt_tree$tip.label <- gsub("^", 'Anilios ', sub_mt_tree$tip.label) # rename so it so we have tips labels without numbers
-
-
-## Rename the outgroups
-sub_mt_tree$tip.label[which(sub_mt_tree$tip.label == 'Anilios acuticauduso')] <- 'Ramphotyphlops acuticaudus'
-sub_mt_tree$tip.label[which(sub_mt_tree$tip.label == 'Anilios suboculariso')] <- 'Acutotyphlops subocularis'
-
-
-# FILTER DATA TO MATCH TREE TIPS -------------------------------------------
-
-tree_tips_df <- as.data.frame(sub_mt_tree$tip.label)
-names(tree_tips_df) <- 'species'
-
-# tree_tips_df$species_in_tree <- sub('Anilios ', tree_tips_df$species, replacement = "", perl = TRUE)
-
-# subset only species in tree
-
-subset_samp <- samptest %>% 
-  dplyr::semi_join(tree_tips_df, by = 'species') %>% 
-  group_by(species) %>% 
-  count()
-
-
-# prune tree again for only tips that we have data for
-shape_data_tips <- as.vector()
-
-# PRUNED tree
-subset_mt_tree <- sub_mt_tree %>%
-  keep.tip(., subset_samp$species)
-
-write.nexus(subset_mt_tree, file = 'data/tree/tree.nwk')
