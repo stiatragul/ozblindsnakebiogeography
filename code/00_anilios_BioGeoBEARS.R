d17b8e862b768367f1a2434e9a56a02e5a8d8213
ST <feathertailglidr@gmail.com>
2022-06-23 16:49:46 +1000

    result table code

--- code/00_anilios_BioGeoBEARS.R
+++ code/00_anilios_BioGeoBEARS.R
@@ -14,6 +14,8 @@ library(BioGeoBEARS)
 
 tree.c <- ape::read.tree('data/tree/anilios_newick_st.tre')
 
+plot(tree.c)
+
 trfn <- "data/tree/anilios_newick_st.tre"
 geogfn <- "data/bears_txt/geofile.txt"
 # adj <- "data/bears_txt/area_adj.txt"
@@ -197,7 +199,7 @@ if (runslow)
 #######################################################
 # PDF plots
 #######################################################
-pdffn = "test.pdf"
+pdffn = "Anilios_DEC.pdf"
 pdf(pdffn, width=6, height=6)
 
 #######################################################
@@ -536,3 +538,234 @@ BAYESj$use_optimx = "GenSA"    # if FALSE, use optim() instead of optimx()
 BAYESj$num_cores_to_use = 4
 BAYESj$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
 BAYES$states_list = states_list # Need to define manually
+
+BAYESj = readfiles_BioGeoBEARS_run(BAYESj)
+BAYESj = section_the_tree(inputs=BAYESj, make_master_table=TRUE, plot_pieces=FALSE)
+
+# Good default settings to get ancestral states
+BAYESj$return_condlikes_table = TRUE
+BAYESj$calc_TTL_loglike_from_condlikes_table = TRUE
+BAYESj$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+dstart = resBAYAREALIKE$outputs@params_table["d","est"]
+estart = resBAYAREALIKE$outputs@params_table["e","est"]
+jstart = 0.0001
+
+# Set up BAYAREALIKE model
+# No subset sympatry
+BAYESj$BioGeoBEARS_model_object@params_table["s","type"] = "fixed"
+BAYESj$BioGeoBEARS_model_object@params_table["s","init"] = 0.0
+BAYESj$BioGeoBEARS_model_object@params_table["s","est"] = 0.0
+
+BAYESj$BioGeoBEARS_model_object@params_table["d","init"] = dstart
+BAYESj$BioGeoBEARS_model_object@params_table["d","est"] = dstart
+BAYESj$BioGeoBEARS_model_object@params_table["e","init"] = estart
+BAYESj$BioGeoBEARS_model_object@params_table["e","est"] = estart
+
+# No vicariance
+BAYESj$BioGeoBEARS_model_object@params_table["v","type"] = "fixed"
+BAYESj$BioGeoBEARS_model_object@params_table["v","init"] = 0.0
+BAYESj$BioGeoBEARS_model_object@params_table["v","est"] = 0.0
+
+# *DO* allow jump dispersal/founder-event speciation (set the starting value close to 0)
+BAYESj$BioGeoBEARS_model_object@params_table["j","type"] = "free"
+BAYESj$BioGeoBEARS_model_object@params_table["j","init"] = jstart
+BAYESj$BioGeoBEARS_model_object@params_table["j","est"] = jstart
+
+# Under BAYAREALIKE+J, the max of "j" should be 1, not 3 (as is default in DEC+J) or 2 (as in DIVALIKE+J)
+BAYESj$BioGeoBEARS_model_object@params_table["j","max"] = 0.99999
+
+# Adjust linkage between parameters
+BAYESj$BioGeoBEARS_model_object@params_table["ysv","type"] = "1-j"
+BAYESj$BioGeoBEARS_model_object@params_table["ys","type"] = "ysv*1/1"
+BAYESj$BioGeoBEARS_model_object@params_table["y","type"] = "1-j"
+
+# Only sympatric/range-copying (y) events allowed, and with 
+# exact copying (both descendants always the same size as the ancestor)
+BAYESj$BioGeoBEARS_model_object@params_table["mx01y","type"] = "fixed"
+BAYESj$BioGeoBEARS_model_object@params_table["mx01y","init"] = 0.9999
+BAYESj$BioGeoBEARS_model_object@params_table["mx01y","est"] = 0.9999
+
+BAYESj$BioGeoBEARS_model_object@params_table["d","min"] = 0.0000001
+BAYESj$BioGeoBEARS_model_object@params_table["d","max"] = 4.9999999
+
+BAYESj$BioGeoBEARS_model_object@params_table["e","min"] = 0.0000001
+BAYESj$BioGeoBEARS_model_object@params_table["e","max"] = 4.9999999
+
+BAYESj$BioGeoBEARS_model_object@params_table["j","min"] = 0.00001
+BAYESj$BioGeoBEARS_model_object@params_table["j","max"] = 0.99999
+
+# Check the inputs
+check_BioGeoBEARS_run(BAYESj)
+
+runslow = TRUE
+resfnbayesj = "AniliosBAYES_J.Rdata"
+if (runslow)
+{
+  resbayesj = bears_optim_run(BAYESj)
+  resbayesj    
+  
+  save(resbayesj, file=resfnbayesj)
+  resBAYAREALIKEj = resbayesj
+} else {
+  # Loads to "res"
+  load(resfnbayesj)
+  resBAYAREALIKEj = resbayesj
+}
+
+pdffn = "Anilios_BAYAREA.pdf"
+pdf(pdffn, width=6, height=6)
+
+#######################################################
+# Plot ancestral states - BayAREALIKE
+#######################################################
+analysis_titletxt ="BioGeoBEARS BAYAREALIKE on Anilios"
+
+# Setup
+results_object = resBAYAREALIKE
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+
+# States
+res2 = plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="text", 
+                                label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, 
+                                cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tree.c, tipranges=tipranges)
+
+# Pie chart
+plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="pie",
+                         label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, 
+                         cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tree.c, tipranges=tipranges)
+
+#######################################################
+# Plot ancestral states - BAYAREALIKE+J
+#######################################################
+analysis_titletxt ="BioGeoBEARS BAYAREALIKE+J on Anilios"
+
+# Setup
+results_object = resBAYAREALIKEj
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+
+# States
+res1 = plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="text", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tree.c, tipranges=tipranges)
+
+# Pie chart
+plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="pie", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tree.c, tipranges=tipranges)
+
+dev.off()
+cmdstr = paste("open ", pdffn, sep="")
+system(cmdstr)
+
+
+
+
+
+
+
+##############
+#####TEST#####
+##############
+
+restable = NULL
+teststable = NULL
+
+## Result table ##
+
+fin.dec = extract_params_from_BioGeoBEARS_results_object(results_object=resDEC, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+fin.decj = extract_params_from_BioGeoBEARS_results_object(results_object=resDECj, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+# fin.decx = extract_params_from_BioGeoBEARS_results_object(results_object=resDECx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+# fin.decjx = extract_params_from_BioGeoBEARS_results_object(results_object=resDECjx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+fin.diva = extract_params_from_BioGeoBEARS_results_object(results_object=resDIVALIKE, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+fin.divaj = extract_params_from_BioGeoBEARS_results_object(results_object=resDIVALIKEj, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+# fin.divax = extract_params_from_BioGeoBEARS_results_object(results_object=resDIVALIKEx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+# fin.divajx = extract_params_from_BioGeoBEARS_results_object(results_object=resDIVALIKEjx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+fin.bayes = extract_params_from_BioGeoBEARS_results_object(results_object=resBAYAREALIKE, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+fin.bayesj = extract_params_from_BioGeoBEARS_results_object(results_object=resBAYAREALIKEj, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+# fin.bayesx = extract_params_from_BioGeoBEARS_results_object(results_object=resBAYAREALIKEx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+# fin.bayesjx = extract_params_from_BioGeoBEARS_results_object(results_object=resBAYAREALIKEjx, returnwhat="table", addl_params=c("j","x"), paramsstr_digits=4)
+
+restable <- rbind(fin.dec,fin.decj, fin.diva,fin.divaj, fin.bayes,fin.bayesj)
+row.names(restable) = c("fin.dec","fin.decj", "fin.diva","fin.divaj","fin.bayes","fin.bayesj")
+restable = put_jcol_after_ecol(restable)
+
+arb <- ape::read.tree('data/tree/anilios_newick_st.tre')
+restable$AICc <- calc_AICc_vals(LnL_vals=restable$LnL,nparam_vals=restable$numparams,samplesize=length(arb$tip.label))
+
+save(restable, file="restable.Rdata")
+# load("restable.Rdata")
+
+## Test Table ##
+aicc.vals <- restable$AICc
+names(aicc.vals) <- rownames(restable)
+
+(aicc.pref <- names(sort(aicc.vals))[1])
+
+lrt.fin <- lrttest(LnL_1= restable$LnL[which(rownames(restable)=="fin.decj")],
+                   LnL_2= restable$LnL[which(rownames(restable)=="fin.dec")],
+                   numparams1= restable$numparams[which(rownames(restable)=="fin.decj")],
+                   numparams2= restable$numparams[which(rownames(restable)=="fin.dec")],
+                   returnwhat = "all")
+
+lrt.fin2 <- lrttest(LnL_1= restable$LnL[which(rownames(restable)=="fin.divaj")],
+                    LnL_2= restable$LnL[which(rownames(restable)=="fin.diva")],
+                    numparams1= restable$numparams[which(rownames(restable)=="fin.divaj")],
+                    numparams2= restable$numparams[which(rownames(restable)=="fin.diva")],
+                    returnwhat = "all")
+
+lrt.fin3 <- lrttest(LnL_1= restable$LnL[which(rownames(restable)=="fin.bayesj")],
+                    LnL_2= restable$LnL[which(rownames(restable)=="fin.bayes")],
+                    numparams1= restable$numparams[which(rownames(restable)=="fin.bayesj")],
+                    numparams2= restable$numparams[which(rownames(restable)=="fin.bayes")],
+                    returnwhat = "all")
+
+result_table <- rbind(lrt.fin, lrt.fin2, lrt.fin3)
+
+result_table$alt <- c("DECj", "DIVAj", "BAYAREAj")
+result_table$null <- c("DEC", "DIVA", "BAYAREA")
+
+result_table$LnLalt <- as.numeric(result_table$LnLalt) %>% round(., digits = 2)
+result_table$LnLnull <- as.numeric(result_table$LnLnull) %>% round(., digits = 2)
+result_table$Dstatistic <- as.numeric(result_table$Dstatistic) %>% round(., digit = 3)
+result_table$pval <- as.numeric(result_table$pval) %>% round(., digit = 4)
+
+result_table
+
+save(result_table, file = "result_table_prelim.Rdata")
+
+### More details 
+lrt_tester <- function(res_alt, res_null, ip_1, ip_2){
+  lnl_alt = get_LnL_from_BioGeoBEARS_results_object(res_alt)
+  lnl_null = get_LnL_from_BioGeoBEARS_results_object(res_null)
+  f_numparams1 = ip_1;  f_numparams2 = ip_2
+  alt_name <- gsub(x = deparse(substitute(res_alt)), pattern = "res", replacement = "")
+  null_name <- gsub(x = deparse(substitute(res_null)), pattern = "res", replacement = "")
+  
+  stats = AICstats_2models(lnl_alt, lnl_null, f_numparams1, f_numparams2)
+  
+  # Null model
+  f_res2 = extract_params_from_BioGeoBEARS_results_object(results_object=res_null, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+  # Alternative model
+  f_res1 = extract_params_from_BioGeoBEARS_results_object(results_object=res_alt, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+  
+  stats$null <- null_name; stats$alt <- alt_name
+  
+  stats[, 3:9] <- as.numeric(stats[, 3:9])
+  
+  return(stats)
+}
+
+
+lrt_DEC <- lrt_tester(res_alt =  resDECj, res_null = resDEC, 
+                      ip_1 = restable$numparams[which(rownames(restable)=="fin.decj")],
+                      ip_2 = restable$numparams[which(rownames(restable)=="fin.dec")]
+)
+
+lrt_DIVA <- lrt_tester(res_alt =  resDIVALIKEj, res_null = resDIVALIKE, 
+                       ip_1 = restable$numparams[which(rownames(restable)=="fin.divaj")],
+                       ip_2 = restable$numparams[which(rownames(restable)=="fin.diva")]
+)
+
+lrt_BAYAREA <- lrt_tester(res_alt =  resBAYAREALIKEj, res_null = resBAYAREALIKE, 
+                          ip_1 = restable$numparams[which(rownames(restable)=="fin.bayesj")],
+                          ip_2 = restable$numparams[which(rownames(restable)=="fin.bayes")]
+)
+
+rbind(lrt_DEC, lrt_DIVA, lrt_BAYAREA)
