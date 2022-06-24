d11b4a0f0c132da9bda789d2b176a133a457c48c
ST <feathertailglidr@gmail.com>
2022-06-24 11:43:31 +1000

    gene concordance factor

--- code/utility/concordance_factor_plot.R
+++ code/utility/concordance_factor_plot.R
@@ -4,7 +4,7 @@ library(patchwork)
 source('code/utility/color_by_CF_Ian.R')
 
 # read in the CF file and appropriate tree (ASTRAL)
-support = read.delim("data/tree/concord_ahe.cf.stat", header=T, comment.char="#") # astral tree
+support = read.delim("data/tree/concord.cf.stat", header=T, comment.char="#") # astral tree
 astral.tree <- read.tree("data/tree/20220617_all_loci.tre")
 
 # Visualize the concordance factors as branch colors
