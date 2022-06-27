a0b4ab9609689dec268f20323ad194a764eb0359
ST <feathertailglidr@gmail.com>
2022-06-27 18:01:10 +1000

    plot time calibrated tree with density

--- code/utility/plot_mcmctreeR.R
+++ code/utility/plot_mcmctreeR.R
@@ -33,60 +33,86 @@ MCMC.tree.plot(phy_full_mcmc, analysis.type = "MCMCtree", cex.tips = 0.6,
 # dev.off()
 
 
+# Plot with posterior -----------------------------------------------------
+
+MCMC.chain <- MCMC.chain[c(1, grep(x = colnames(MCMC.chain), pattern = 't_n'))]
+phy.edge <- phy_full$edge
+
+# extract ages with node age posteriors from column 2
+MCMC.node.ages <- MCMC.chain[, 2:Ntip(phy_full)]
+
+# extract the names from these data so they are numeric node
+# labels that match the APE tree format
+all.nodes <- as.numeric(gsub("t_n", "", colnames(MCMC.node.ages)))
+
+# create a vector of names for each list element as internal
+# nodes from APE tree, using phy$edge object.
+node.ages.names <- c(Ntip(phy_full) + 1, phy.edge[which(phy.edge[, 2] > Ntip(phy_full)), 2])
+# find where each posterior node age appears in APE edge
+# object.
+match.nodes <- match(all.nodes, as.numeric(node.ages.names))
+# create a list extracting the information from the MCMC
+# chain in APE node order.
+node.ages <- lapply(match.nodes, function(uu) MCMC.node.ages[, 
+                                                             uu])
+# name each element in list.
+names(node.ages) <- node.ages.names
+
+dev.off()
+pdf(file = 'output/mcmctree_full_age_distribution.pdf', paper = "a4" )
+MCMC.tree.plot(phy = phy_full, node.ages = node.ages, analysis.type = "user", 
+               cex.tips = 0.2, time.correction = 100, scale.res = c("Eon", 
+                                                                    "Period"), plot.type = "distributions", cex.age = 0.4, 
+               cex.labels = 0.5, relative.height = 0.08, col.tree = "grey40", 
+               no.margin = TRUE)
+
+dev.off()
+
 # Subset species tree to Anilios ------------------------------------------
 
-# tips we want for blindsnakes (the best we can do since we do not have all described species)
-blindsnakes_samples <- c('Anilios_affinis_J93635','Anilios_ammodytes_R158097','Anilios_aspina_J91822','Anilios_australis_R115859',
-                         'Anilios_bicolor_R55342','Anilios_bituberculatus_R63990','Anilios_broomi_J87805','Anilios_centralis_R14631',
-                         'Anilios_diversus_R112027','Anilios_endoterus_R102725','Anilios_ganei_R165000', 
-                         'Anilios_grypusW_R108596',   # Western Australia (W)
-                         'Anilios_grypusNW_R157297',   # Sister to leptosoma-complex (NW)
-                         'Anilios_grypusET_R55272',    # Eastern (ET)
-                         # 'Anilios_grypus_R108596',   # Western Australia (W)
-                         # 'Anilios_grypus_R157297',   # Sister to leptosoma-complex (NW)
-                         # 'Anilios_grypus_R55272',    # Eastern (ET)
-                         'Anilios_guentheri_R108431','Anilios_hamatus_R136277','Anilios_howi_R146381','Anilios_kimberleyensis_R164213', 
-                         'Anilios_leptosoma_R119241','Anilios_leucoproctus_J87547', 
-                         'Anilios_ligatus_R31019','Anilios_longissimus_R120049','Anilios_margaretae_R163269',
-                         'Anilios_nigrescens_R31022','Anilios_obtusifrons_R146400','Anilios_pilbarensis_R108813',
-                         'Anilios_pinguis_R146995', 'Anilios_proximus_R132486', 'Anilios_silvia_J46128','Anilios_splendidus_R119900','Anilios_systenos_R114894', 
-                         'Anilios_torresianus_J47640','Anilios_tovelli_R21063','Anilios_troglodytes_R146048', 
-                         'Anilios_unguirostris_R21669','Anilios_waitii_R113302','Anilios_wiedii_J59020', 
-                         # 'Anilios_ligatus_R19109', # eastern
-                         'Anilios_yirrikalae_J85695'
-                         # 'Ramphotyphlops_cfwaitii_R51244', #omit
-)
+plot(phy_full)
+nodelabels()
 
-outgroups <- c('Sundatyphlops_polygrammicus_R98715', 'Acutotyphlops_subocularis_R64768', 'Ramphotyphlops_multilineatus_R148379')
-# c('Ramphotyphlops_multillineatus_ABTC148379', 'Acutotyphlops_subocularis_R64768', 'Anilios_polygrammicus_R98715')
+phy_small <- ape::extract.clade(phy = phy_full, 159)
 
-# Subset ------------------------------------------------------------------
-# Subset the tree
+plot(phy_small)
+nodelabels()
 
-phy_small <- ape::keep.tip(phy = phy_full, tip = c(blindsnakes_samples, outgroups))
+# 
+# node_ages <- as.data.frame(phy_full_mcmc$nodeAges)
+# node_ages$row <- rownames(node_ages)
 
-head(MCMC.chain)
-phy.edge <- phy_small$edge
 
-MCMCtree.posterior
-
-node_ages <- as.data.frame(phy_full_mcmc$nodeAges)
-node_ages$row <- rownames(node_ages)
-subset_node_age <- node_ages[which(node_ages$row >= 159), ]
+phy_small$edge
+node.numbers <- c(47:91) # corresponds to 159:203 in the original phylogeny. This is without taking away any internal taxa.
+node.numbers_old <- c(159:203)
 
 # create a list to store each posterior sample for every node
 node.posteriors <- vector(mode = "list", length = Nnode(phy_small))
-
-node.numbers <- c(47:91)
 names(node.posteriors) <- node.numbers
-for (i in 1:Nnode(phy_small)) node.posteriors[[i]] <- rnorm(1000, 
-                                                            mean = subset_node_age$mean[i], sd = 0.1)
 
-node.posteriors
+node.ages$`159`
 
-MCMC.tree.plot(phy = phy_small, analysis.type = "user", node.ages = node.posteriors,
-               cex.tips = 0.7, time.correction = 100, 
-               scale.res = c("Eon", "Period"), plot.type = "distributions")
+#  list of posterior from full analysis
+class(node.ages)
 
+match.nodes_s <- match(node.numbers_old, as.numeric(node.ages.names))
+node.ages_new <- lapply(match.nodes_s, function(uu) MCMC.node.ages[, uu])
+
+names(node.ages_new) <- as.character(node.numbers)
+
+names(node.ages_new)
+
+
+# dev.off()
+# pdf(file = 'output/mcmctree_anilios_age_bar.pdf', paper = "a4" )
+MCMC.tree.plot(phy = phy_small, node.ages = node.ages_new, analysis.type = "user", 
+               cex.tips = 0.6, time.correction = 100, 
+               scale.res = c("Eon", "Period"), plot.type = "phylogram", 
+               cex.age = 0.6, lwd.bar = 1, cex.labels = 0.6, 
+               relative.height = 0.08, col.tree = "grey40", col.age = "navy",
+               node.method = "bar",
+               no.margin = TRUE)
+# dev.off()
 
 
