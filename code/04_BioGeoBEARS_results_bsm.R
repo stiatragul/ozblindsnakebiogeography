4ed6581a4501ae712eeeff4cf7f6943ca40f4139
Putter Tiatragul <stiatragul@live.com>
2022-11-17 15:03:52 +1100

    move plotting section to new script

--- code/04_BioGeoBEARS_results_bsm.R
+++ code/04_BioGeoBEARS_results_bsm.R
@@ -13,7 +13,6 @@ library(rexpokit);library(cladoRcpp); library(BioGeoBEARS)
 # library(dplyr)
 library(parallel); library(snow)
 
-
 # Tree --------------------------------------------------------------------
 
 tree.c <- ape::read.tree('data/intermediate_data/bears/blindsnake_b.tre')
@@ -22,7 +21,6 @@ tree.c$edge.length <- tree.c$edge.length * 100
 # Saved results -----------------------------------------------------------
 
 load("data/intermediate_data/bears/bears_model_fit.Rdata")
-
 
 ############################
 #####Stochastic mapping#####
@@ -58,9 +56,6 @@ plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"
 dev.off()  # Turn off PDF
 cmdstr = paste("open ", pdffn, sep="")
 system(cmdstr) # Plot it
-
-
-
 
 #######################################################
 # Stochastic mapping on DEC+J+x
@@ -232,7 +227,7 @@ actual_names
 dmat_times = get_dmat_times_from_res(res=res, numstates=NULL)
 dmat_times
 
-# Extract BSM output
+
 clado_events_tables = BSM_output$RES_clado_events_tables
 ana_events_tables = BSM_output$RES_ana_events_tables
 
@@ -244,169 +239,6 @@ ana_events_tables = BSMs_w_sourceAreas$ana_events_tables
 # Count all anagenetic and cladogenetic events
 counts_list = count_ana_clado_events(clado_events_tables, ana_events_tables,
                                      areanames, actual_names)
-
-summary_counts_BSMs = counts_list$summary_counts_BSMs
-print(conditional_format_table(summary_counts_BSMs))
-
-# Histogram of event counts
-hist_event_counts(counts_list, pdffn=paste0("output/", model_name, "_histograms_of_event_counts.pdf"))
-
-
-# Count individual stuff --------------------------------------------------
-
-### Sum up dispersal event to different locations ###
-
-# Name each column by letter of each biome LETTERS[1:length(number_of_areas))]
-colnames(counts_list$anagenetic_dispersals_counts_cube) <- LETTERS[1:9]
-
-# Find sum of each column (to get gross number of dispersal TO each biome per stochastic map)
-sum_disp_area <- rowSums(counts_list$anagenetic_dispersals_counts_cube, dims = 2)
-
-counts_list$anagenetic_dispersals_counts_cube
-
-rownames(sum_disp_area) <- LETTERS[1:9]
-sum_disp_area_df <- data.frame(sum_disp_area)
-sum_disp_area_df$from <- rownames(sum_disp_area_df)
-
-ana_disp_area_df <- sum_disp_area_df %>% 
-  tidyr::pivot_longer(!from, names_to = "to", values_to = "count") %>% 
-  dplyr::mutate(state_from = ifelse(from == "C", "arid", "non_arid")) %>% 
-  dplyr::mutate(state_to = ifelse(to == "C", "arid", "non_arid")) %>% 
-  dplyr::mutate(subset_from = ifelse(from %in% c("G", "H", "I"), "island", from)) %>% 
-  dplyr::mutate(subset_to = ifelse(to %in% c("G", "H", "I"), "island", to))
-
-
-# plot outward dispersal from - > to
-outward_dispersal <- ggplot(data = ana_disp_area_df, aes(fill= to, y = count, x = from)) + 
-  geom_bar(position="stack", stat="identity") +
-  labs(x = "Outward dispersal", y = "frequency") + 
-  guides(fill = guide_legend(title = "Destination"))
-
-outward_dispersal
-
-# Outward dispersal by geographic state
-ggplot(data = ana_disp_area_df, aes(fill= state_to, y = count, x = state_from)) + 
-  geom_bar(position="stack", stat="identity") +
-  labs(x = "Outward dispersal from", y = "frequency") + 
-  guides(fill = guide_legend(title = "Destination"))
-
-
-ggplot(data = ana_disp_area_df, aes(fill= subset_to, y = count, x = subset_from)) + 
-  geom_bar(position="stack", stat="identity") +
-  labs(x = "Outward dispersal from", y = "frequency") + 
-  guides(fill = guide_legend(title = "Destination"))
-
-
-# Inward dispersal
-inward_disp_area <- colSums(counts_list$anagenetic_dispersals_counts_cube, dims = 2)
-
-
-
-sum_disp_area <- colSums(counts_list$anagenetic_dispersals_counts_cube, dims = 1)
-rowSums(sum_disp_area)
-
-colMeans(counts_list$anagenetic_dispersals_counts_cube, dims = 1)
-
-counts_list$ana_dispersals_counts_fromto_means
-
-dim(counts_list$anagenetic_dispersals_counts_cube)
-
-
-
-
-
-## STATs ##
-library(dplyr)
-library(stringr)
-
-# Check individual data
-clado_events_tables[2][[1]] %>% 
-  dplyr::filter(stringr::str_detect(clado_event_type, pattern = regex("[A-z]+"))) %>% 
-  dplyr::select(clado_event_type, node, ord_ndname, clado_event_txt, clado_dispersal_to, everything()) %>% 
-  tibble()
-
-# Dataframes for each stochastic map labeled
-
-clado_events_tables = BSMs_w_sourceAreas$clado_events_tables # resets
-
-clado_events_tables[1][[1]] %>% class()
-
-for(i in 1:length(clado_events_tables)){
-  
-  clado_events_tables[i][[1]]$stochastic_no <- paste0("s", i) 
-  
-}
-
-# merge the n number of dataframes together
-
-sm_df <- do.call(rbind, clado_events_tables)
-
-# Founder events
-founder_events_df <- sm_df %>%
-  # filter for clado_events we care about
-  dplyr::filter(str_detect(clado_event_type, pattern = regex("(j)"))) %>% 
-  # select columns that we want 
-  dplyr::select(clado_event_type, node, ord_ndname, clado_event_txt, clado_dispersal_to, stochastic_no, everything()) %>% 
-  dplyr::mutate(from = str_extract(clado_event_txt, pattern = "[A-Z]")) %>% 
-  dplyr::mutate(transitions = paste0(from, "-", clado_dispersal_to)) %>% 
-  dplyr::group_by(node, transitions) %>% 
-  dplyr::summarise(n = n()) %>% 
-  dplyr::mutate(freq = n / sum(n)) 
-
-founder_events_df$node <- as.character(founder_events_df$node)
-
-# Get rid of those with one transition
-
-founder_events_df
-
-
-plot(tr)
-nodelabels()
-
-obj <- founder_events_df %>% 
-  dplyr::select(node, transitions, freq) %>% 
-  tidyr::pivot_wider(names_from = transitions, values_from = freq) 
-
-# replace NA with 0
-obj[is.na(obj)] <- 0
-
-obj[-1]
-
-pie_matrix <- as.matrix(obj[-1])
-rownames(pie_matrix) <- obj$node
-
-pie_matrix
-
-dim(pie_matrix)[2]
-
-library(viridis)
-cols<-setNames(viridis(dim(pie_matrix)[2]), colnames(pie_matrix))
-
-dev.off()
-plot(tr)
-nodelabels(pie=pie_matrix, piecol=cols,cex=0.5)
-legend(x="bottomright", legend=colnames(pie_matrix), 
-       pch=21,pt.cex=2,pt.bg=cols,cex=1)
-
-pie_df <- founder_events_df %>% 
-  dplyr::select(node, transitions, freq) %>% 
-  tidyr::pivot_wider(names_from = transitions, values_from = freq) %>% 
-  tidyr::pivot_longer(!node, names_to = "props") %>% 
-  mutate(ypos = cumsum(value)- 0.5*value)
-
-pie_df
-
-library(ggplot2)
-
-pie_charts_order_proportion <- ggplot(data=pie_df, aes(x=" ", y=value, group=node, colour=props, fill=props)) +
-  geom_bar(width = 1, stat = "identity", colour = "white") +
-  # scale_fill_manual("Proportions", values=c("#648FFF", "#FFB000")) +
-  geom_text(aes(y = ypos, label = props), position = position_stack(vjust = 0.5), color = "white")  +
-  coord_polar("y") + 
-  facet_wrap(~ node) +theme_void() 
-
-pie_charts_order_proportion 
-
 
 #######################################################
 # Print counts to files
@@ -445,5 +277,7 @@ library(MultinomialCI)    # For 95% CIs on BSM counts
 check_ML_vs_BSM(res, clado_events_tables, model_name, tr=NULL, plot_each_node=FALSE, linreg_plot=TRUE, MultinomialCI=TRUE)
 
 
+# SAVE
 
+save.image(file = 'data/intermediate_data/bears/BSB_50.Rdata')
 
