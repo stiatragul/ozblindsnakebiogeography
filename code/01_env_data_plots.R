c4a6fd1056e919b5794a5687f8482ce4df1bcac1
Putter Tiatragul <stiatragul@live.com>
2023-01-23 17:36:58 +1100

    change colours for arid and temperature plots

--- code/01_env_data_plots.R
+++ code/01_env_data_plots.R
@@ -33,11 +33,11 @@ env_plottr <- function(.df){
   .df %>% 
   ggplot(data = ., aes(x = age, colour = source)) +
     geom_line(aes(y = vals), size = 1) +
-    geom_point(aes(y = vals)) + 
+    # geom_point(aes(y = vals)) + 
     deeptime::coord_geo(xlim = c(26, 0), dat = c("epochs"), pos = 'bottom', expand = TRUE) +
     # scale_colour_manual(values = viridis(4), name = "Data source", labels = c("RPANDA Global", "Scotese Australia", "Straume Australia", "Valdes Australia")) +
     scale_y_continuous(limits = c(-5, 30), breaks = seq(-5, 30, 5)) + labs(y = "Mean temperature (°C)") +
-    scale_x_reverse("Age (Ma)", limits = c(25, 0), breaks = seq(25, 0, by = -5)) + theme_bw() 
+    scale_x_reverse("Time (Ma)", limits = c(25, 0), breaks = seq(25, 0, by = -5)) + theme_bw() 
 }
 # env_plottr <- function(.df){
 #   .df %>% 
@@ -60,14 +60,33 @@ env_df[which(env_df$source %in% c("mean_val", "max_val", "min_val")),] %>% env_p
 
 dev.off()  
 
-temp_plots <- env_df[which(env_df$source %in% c("mean_sco", "mean_str", "mean_val", "global_temp")),] %>% env_plottr()
+# temp_plots <- env_df[which(env_df$source %in% c("mean_sco", "mean_str", "mean_val", "global_temp")),] %>% env_plottr()
 
-arid_plots <- env_df[which(env_df$source %in% c("arid_sco", "arid_str", "arid_val")),] %>% 
+
+temp_plots <- env_df[which(env_df$source %in% c("mean_sco", "mean_str", "mean_val", "global_temp")),] %>% 
   ggplot(data = ., aes(x = age, colour = source)) +
   geom_line(aes(y = vals), size = 1) +
-  scale_x_reverse("Age (Ma)", limits = c(25, 0), breaks = seq(25, 0, by = -5)) + theme_bw() +
+  # geom_point(aes(y = vals)) + 
   deeptime::coord_geo(xlim = c(26, 0), dat = c("epochs"), pos = 'bottom', expand = TRUE) +
-  labs(y = "Aridity index")
+  scale_colour_manual(values = c("#F0027F", "#386CB0", "#FDC086", "#666666"), name = "Data source", labels = c("RPANDA Global", "Scotese and Wright 2018", "Straume et al. 2019", "Valdes et al. 2021")) +
+  # scale_color_brewer(type='qual',palette='Set1', name = "Data source", labels = c("RPANDA Global", "Scotese and Wright 2018", "Straume et al. 2019", "Valdes et al. 2021")) +
+  scale_y_continuous(limits = c(-5, 30), breaks = seq(-5, 30, 5)) + labs(y = "Mean temperature (°C)") +
+  scale_x_reverse("Time (Ma)", limits = c(25, 0), breaks = seq(25, 0, by = -5)) + theme_bw() +
+  theme(panel.border = element_blank(), axis.line.x = element_line(color = "black", size = 0.5), axis.line.y = element_line(color = "black", size = 0.5)) +
+  theme(legend.position = "right")
+
+
+arid_plots <-env_df[which(env_df$source %in% c("arid_sco", "arid_str", "arid_val")),] %>% 
+  ggplot(data = ., aes(x = age, colour = source)) +
+  geom_line(aes(y = vals), size = 1) +
+  scale_x_reverse("Time (Ma)", limits = c(25, 0), breaks = seq(25, 0, by = -5)) + theme_bw() +
+  deeptime::coord_geo(xlim = c(26, 0), dat = c("epochs"), pos = 'bottom', expand = TRUE) +
+  labs(y = "Aridity index") +
+  # scale_color_brewer(type='qual',palette='Set1') +
+  # scale_color_brewer(type='qual',palette='Set1', name = "Data source", labels = c("Scotese and Wright 2018", "Straume et al. 2019", "Valdes et al. 2021")) +
+  scale_colour_manual(values = c("#386CB0", "#FDC086", "#666666"), name = "Data source", labels = c("Scotese and Wright 2018", "Straume et al. 2019", "Valdes et al. 2021")) +
+  theme(panel.border = element_blank(), axis.line.x = element_line(color = "black", size = 0.5), axis.line.y = element_line(color = "black", size = 0.5)) +
+  theme(legend.position = "right")
 
 library(patchwork)
 
